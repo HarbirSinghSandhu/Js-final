e5dc062f2bbf72742bcc74780effebe8a5ba3c02
Harbir Singh Sandhu <102942308+HarbirSinghSandhu@users.noreply.github.com>
2024-06-25 21:44:07 +0000

    Comparison.js

---
+++ .devcontainer/basics/Comparison.js
@@ -0,0 +1,17 @@
+//console.log(2>1);
+//console.log(3<=1)
+
+
+//console.log("2" >1 ); //Dat Type Should Be same
+//console.log("03">1);
+
+//console.log(null>0);
+//console.log(null == 0);
+//console.log(null >=0);
+
+//console.log(undefined==0);
+//console.log(undefined>0);
+//console.log(undefined<0);
+
+//=== Strict check which means it also checks the data type
+console.log("2"===2);
