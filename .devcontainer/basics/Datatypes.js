11a21bd5511ca0b1b6655634930ddfa4b6058ec3
Harbir Singh Sandhu <102942308+HarbirSinghSandhu@users.noreply.github.com>
2024-06-25 20:28:06 +0000

    variables.js

---
+++ .devcontainer/basics/Datatypes.js
@@ -0,0 +1,5 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+4) //Not works in nodejs, only works on browser
+console.log(3+4)
+console.log("sandhu") 
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 985600
+let accountEmail = "hsandhu0015@gmail.com"
+accountPassword ="12345"
+accountCity ="Jalandhar"
+
+ccountId=2
+AccountEmail ="hsandhu1@laurentian.ca"
+accountPassword ="546789"
+accountCity ="Batala"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
