e673e88b77f3c7fb451e6ea05bd0b48730f08790
Harbir Singh Sandhu <102942308+HarbirSinghSandhu@users.noreply.github.com>
2024-06-25 21:01:55 +0000

    Conversionoperation.js

---
+++ .devcontainer/basics/Conversionoperation.js
@@ -0,0 +1,24 @@
+//let age=object
+
+//console.log(typeof age);
+//console.log(typeof(age));
+
+//let valueinnumber =Number(age)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+//"26"=26
+//"26abc"=> NaN
+//true =. 1; false=0
+
+let isloggedin = 1
+let booleanisloggedin = Boolean(isloggedin)
+//console.log(booleanisloggedin);
+// 1=.true
+//0 =false
+//Name stringh"harbir" =true
+
+let number = 7
+let stringnumber = String(number);
+console.log(stringnumber);
+console.log(typeof stringnumber);
