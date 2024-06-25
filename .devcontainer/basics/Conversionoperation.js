4e3af6889bff0ad840168c5e558a0c478210ad46
Harbir Singh Sandhu <102942308+HarbirSinghSandhu@users.noreply.github.com>
2024-06-25 21:32:41 +0000

    Conversionoperation.js

--- .devcontainer/basics/Conversionoperation.js
+++ .devcontainer/basics/Conversionoperation.js
@@ -20,5 +20,26 @@ let booleanisloggedin = Boolean(isloggedin)
 
 let number = 7
 let stringnumber = String(number);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+//console.log(stringnumber);
+//console.log(typeof stringnumber);
+
+
+
+
+//*******************0peration **************
+
+let value=7
+let negvalue = -value
+//console.log(negvalue);
+//console.log(7*8);
+
+let str1 = "Sat";
+let str2 = " Shri";
+let str3 = " AKal" ;
+let str4 = str1 + str2 + str3
+//console.log(str4);
+
+let gamecounter =50;
+gamecounter++;
+console.log(gamecounter);  
+
